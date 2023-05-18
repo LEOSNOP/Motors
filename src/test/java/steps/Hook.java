2e577430d270a732345d4c96b1acae15f5224a56
Leo <conceicao.leonardo9@gmail.com>
2023-05-18 12:18:24 -0300

    feature e hook

---
+++ src/test/java/steps/Hook.java
@@ -0,0 +1,23 @@
+package steps;
+
+import basePages.BaseStep;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class Hook {
+
+    @Before
+    public void BeforeAllTest() throws Throwable {
+        BaseStep.abrindo_nav();
+    }
+
+    @After
+    public void AfterAllTest(Scenario scenario) throws Throwable {
+        //BaseStep.Screenshot("Fim do test/"+ scenario.getId());
+        BaseStep.saindo_nav();
+    }
+
+
+
+}
