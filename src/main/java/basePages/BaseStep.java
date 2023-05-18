5e4894f03da5656bc78e4ebcbb53c1b431467b44
Leo <conceicao.leonardo9@gmail.com>
2023-05-18 14:08:17 -0300

    Runner, steps configurado

--- src/main/java/basePages/BaseStep.java
+++ src/main/java/basePages/BaseStep.java
@@ -5,16 +5,22 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.IOException;
 
 public class BaseStep {
+
     public static WebDriver driver;
 
     public static void abrindo_nav()throws Throwable{
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
         System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
