de565471c0612b31548c1ef9820ca26e406fbbce
Leo <conceicao.leonardo9@gmail.com>
2023-05-18 14:07:56 -0300

    Runner, steps configurado

---
+++ src/test/java/steps/MotorSteps.java
@@ -0,0 +1,53 @@
+package steps;
+
+import basePages.BaseStep;
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+
+public class MotorSteps extends BaseStep {
+
+    @Dado("^que o usuario acessa a pagina da webMotors$")
+    public void queOUsuarioAcessaAPaginaDaWebMotors() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("^o usuario faz a busca por \"([^\"]*)\"$")
+    public void oUsuarioFazABuscaPor(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Entao("^verifica o resultado da busca pela marca$")
+    public void verificaOResultadoDaBuscaPelaMarca() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Entao("^verifica o resultado da busca pelo modelo$")
+    public void verificaOResultadoDaBuscaPeloModelo() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Entao("^verifica o resultado da busca pela versao$")
+    public void verificaOResultadoDaBuscaPelaVersao() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("^o usuario faz busca \"([^\"]*)\"$")
+    public void oUsuarioFazBusca(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Entao("^o sistema permanece na pagina$")
+    public void oSistemaPermaneceNaPagina() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
