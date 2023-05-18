19f73dd41044391f696e36dd73ac92cc8b10dcf4
Leo <conceicao.leonardo9@gmail.com>
2023-05-18 12:09:32 -0300

    basepages , time sleep configurado

--- src/main/java/entidades/TimeSleep.java
+++ src/main/java/entidades/TimeSleep.java
@@ -1,4 +1,10 @@
 package entidades;
 
 public class TimeSleep {
+
+    public static void timesleep(int time){
+        try{
+            Thread.sleep(time);
+        }catch (Exception e){}
+    }
 }
